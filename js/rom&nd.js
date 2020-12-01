8e5f0cdef4245578a8158745c3fb13c9f9996c8b
Megumi fujino <>
2020-12-01 20:15:32 +0900

    slider追加

---
+++ js/rom&nd.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 10,
+    centeredSlides : true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    breakpoints: {
+      767: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      }
+    }
+  })
