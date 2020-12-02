6d2e52393d918935cf4834a39385b440ee3db5f0
Megumi fujino <>
2020-12-02 16:28:19 +0900

    写真の

--- js/rom&nd.js
+++ js/rom&nd.js
@@ -1,5 +1,21 @@
 var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
+    slidesPerView: 3.8,
+    spaceBetween: 10,
+    centeredSlides : true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    breakpoints: {
+      767: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      }
+    }
+  })
+
+  var mySwiper = new Swiper ('#in', {
+    loop: true,
     slidesPerView: 2,
     spaceBetween: 10,
     centeredSlides : true,
